eb241d26aca453c282385fef15d097fece5b55e3
Prakhar Gupta <prakhargupta448@gmail.com>
2026-08-25 23:28:15 +0530

    Time: 2 ms (68.20%) | Memory: 45.4 MB (38.11%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,22 +1,13 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int num=nums[i];
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        int mull=1;
-        if(nums.length==1){
-            if(nums[0]==k){
-                return k*2;
-            }else{
-                return k;
-            }
+        HashSet<Integer> map=new HashSet<>();
+        for(int num:nums){
+            map.add(num);
         }
         for(int i=1;i<=nums.length+1;i++){
-            mull=i*k;
-            if(!map.containsKey(mull)){
-                return mull;
+            int mul=i*k;
+            if(!map.contains(mul)){
+                return mul;
             }
         }
         return -1;
